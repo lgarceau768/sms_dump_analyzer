eeacd908506f96b46895d74efe01737814b15eb3
unknown <luke.garceau@kavokerr.com>
2019-09-19 12:18:19 -1100

    added some more features

--- readSMSDump.py
+++ readSMSDump.py
@@ -1,6 +1,11 @@
 import os, sys, re, smsClass
-
+from consolemenu import *
+from consolemenu.items import *
+from prettytable import *
+from colorama import init, Fore
 # first message on the first occurance of #
+smsMsgs = []
+init()
 
 '''
 help function
@@ -11,6 +16,57 @@ def help():
     print('\t-f:  \tpath to file (from cwd) %s' % os.getcwd())
     print('\t-o:  \tpath to output (default cwd: %s)' % os.getcwd())
 
+'''
+display all messages command
+'''
+def displayAll():
+    smsTable = PrettyTable()
+    smsTable.field_names = [Fore.BLACK+'Date'+Fore.WHITE, Fore.BLUE+'Type'+Fore.WHITE, Fore.LIGHTCYAN_EX+'Number'+Fore.WHITE, Fore.MAGENTA+'Message'+Fore.WHITE]
+    for sms in smsMsgs:
+        smsTable.add_row([Fore.BLACK+sms.get_date()+Fore.WHITE, Fore.BLUE+sms.get_type()+Fore.WHITE, Fore.LIGHTCYAN_EX+sms.get_number()+Fore.WHITE, Fore.MAGENTA+sms.get_message()+Fore.WHITE])
+    print(smsTable)
+    input('Continue')
+
+'''
+message counter
+'''
+def countMessages():
+    # count all messages
+    # list of the unique numbers
+    numbers = []
+    for sms in smsMsgs:
+        if [sms.get_number(), 0] not in numbers:
+            numbers.append([sms.get_number(), 0])
+    total = len(smsMsgs)
+    for sms in smsMsgs:
+        number = sms.get_number()
+        for i in range(len(numbers)):
+            if number == numbers[i][0]:
+                numbers[i][1] += 1
+    messagesCount = PrettyTable()
+    messagesCount.field_names = [Fore.GREEN+'Number'+Fore.WHITE, Fore.BLUE+'Number of messages'+Fore.WHITE, Fore.RED+'Percent Messaged'+Fore.WHITE]
+    messagesCount.add_row([Fore.GREEN+'Total'+Fore.WHITE, Fore.BLUE+str(total)+Fore.WHITE, Fore.RED+'100%'+Fore.WHITE])
+    def sortSecond(val):
+        return val[1]
+    numbers.sort(key = sortSecond, reverse = True)
+    for number in numbers:
+        percent = number[1] / total
+        percent *= 100
+        percent = str(percent)
+        messagesCount.add_row([Fore.GREEN+number[0]+Fore.WHITE, Fore.BLUE+str(number[1])+Fore.WHITE, Fore.RED+percent+Fore.WHITE])
+    print(messagesCount)
+    input('Continue')
+
+'''
+menu function will return index of method to call, args
+'''
+def menu():
+    menu = ConsoleMenu('SMS Dump Analyzer', 'Main Menu')
+    display = FunctionItem('Display All Messages', displayAll)
+    analytics = FunctionItem('Count Messages', countMessages)
+    menu.append_item(display)
+    menu.append_item(analytics)
+    menu.show()
 
 
 # need to interpret args
@@ -71,15 +127,13 @@ else:
                 i+=second
         smsDump.close()
     
-    smsMsgs = []
     for msg in messages:
         # convert them to SMS variables
         try:
             msg = msg.split('\n')
             date = msg[1].replace('Date: ','')
             type = msg[0].replace('Type: ','')
-            number = msg[2].replace('Address: ','').replace('+1','')
-            number = number[:3]+'-'+number[3:6]+'-'+number[6:]
+            number = msg[2].replace('Address: ','').replace('+1','').replace('(','').replace(')','').replace('-','').replace(' ','').strip()
             message = msg[4].replace('Message: ','').replace('$U$','')
             if '0x' in message:
                 index = message.find('0x')
@@ -92,8 +146,4 @@ else:
     
     smsMsgs.sort(key=smsClass.SMS.get_number, reverse=True)
     
-    number = input('Please enter a number to retrieve the messages: ')
-
-    for sms in smsMsgs:
-        if sms.get_number() == number:
-            print(sms)
+    menu()
